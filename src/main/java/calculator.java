6b1de8daf8ad01d88138430a5a29bf005c9b69b8
bawar17 <bawar17@gmail.com>
2016-04-06 14:58:03 +0200

    första verisionen av min miniräknare...

---
+++ src/main/java/calculator.java
@@ -0,0 +1,24 @@
+
+public class calculator {
+	
+	public double add(double x, double y){
+		
+		double resultat = x + y;
+		return resultat;
+	}
+	
+	public double sub (double x, double y){
+		double resultat = x - y;
+		return resultat;
+	}
+	public double mul(double x, double y){
+		double resultat = x * y;
+		return resultat;
+	}
+	public double div(double x, double y){
+		double resultat = x / y;
+		return resultat;
+	}
+	
+
+}
---
+++ src/main/java/main.java
@@ -0,0 +1,16 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		calculator Calculator = new calculator();
+		
+		System.out.println( Calculator.add( 15.0, 10.0));
+		System.out.println( Calculator.sub(150.0, 100.0));
+		System.out.println( Calculator.mul(1.0, 10.0));
+		System.out.println( Calculator.div(100.0, 2.0));
+		
+	}
+
+}
