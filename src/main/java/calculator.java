fd1162534224b159e4120cb24cc504aaa7fc2e2f
bawar17 <bawar17@gmail.com>
2016-04-06 15:14:17 +0200

    min andra versrionen

--- src/main/java/calculator.java
+++ src/main/java/calculator.java
@@ -2,22 +2,17 @@
 public class calculator {
 	
 	public double add(double x, double y){
-		
-		double resultat = x + y;
-		return resultat;
+				return x+y;
 	}
-	
 	public double sub (double x, double y){
-		double resultat = x - y;
-		return resultat;
+		
+		return x - y;
 	}
 	public double mul(double x, double y){
-		double resultat = x * y;
-		return resultat;
+		return x*y;
 	}
 	public double div(double x, double y){
-		double resultat = x / y;
-		return resultat;
+		return x/y;
 	}
 	
 
